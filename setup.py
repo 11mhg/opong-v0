cded15c38b4aba7650aaab5502ac7185e3f783c9
Hamada Gasmallah <11mhg@queensu.ca>
2019-11-11 11:51:26 -0500

    Finished product

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@ from setuptools import setup
 
 setup(name='gym_opong',
         version='0.0.1',
-        install_requires=['gym','pygame','numpy'])
+        description='Private openai gym implementation of pong with custom state space encoding',
+        author='Mohammed Gasmallah',
+        author_email='11mhg@queensu.ca',
+        licens='unlicense',
+        install_requires=['gym','pygame','numpy'],
+        zip_safe=False
+)
